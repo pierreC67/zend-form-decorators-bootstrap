12b60dce6094e08f338b16540328abff8c24e9c9
pierreC67 <pierre@coustillas.com>
2018-02-26 12:37:42 +0100

    add support file

--- Twitter/Bootstrap/Form/Element/File.php
+++ Twitter/Bootstrap/Form/Element/File.php
@@ -1,53 +1,109 @@
 <?php
 /**
- * A form file definition
+ * Form definition
  *
  * @category Forms
- * @package Twitter_Bootstrap_Form
- * @subpackage Element
+ * @package Twitter_Bootstrap
+ * @subpackage Form
  * @author Christian Soronellas <csoronellas@emagister.com>
  */
 
 /**
- * A form file element
+ * An "horizontal" Twitter Bootstrap's UI form
  *
  * @category Forms
- * @package Twitter_Bootstrap_Form
- * @subpackage Element
+ * @package Twitter_Bootstrap
+ * @subpackage Form
  * @author Christian Soronellas <csoronellas@emagister.com>
  */
-class Twitter_Bootstrap_Form_Element_File extends Zend_Form_Element_File
+class Twitter_Bootstrap_Form_Horizontal extends Twitter_Bootstrap_Form
 {
-    /**
-     * Adds a file decorator if no one found
-     */
-    public function loadDefaultDecorators()
+    public function __construct($options = null)
     {
-        if ($this->loadDefaultDecoratorsIsDisabled()) {
-            return $this;
-        }
+        $this->_initializePrefixes();
 
-        if (!$this->_existsFileDecorator()) {
-            // Add the file decorator to the beginning
-            $decorators = array_merge(array('File'), $this->getDecorators());
-            $this->setDecorators($decorators);
-            $this->removeDecorator('ViewHelper');
-        }
-        return $this;
-    }
+        $this->setDisposition(self::DISPOSITION_HORIZONTAL);
 
-    /**
-     * Checks if a file decorator has been added to the decorators
-     * stack
-     */
-    protected function _existsFileDecorator()
-    {
-        foreach ($this->getDecorators() as $decorator) {
-            if ($decorator instanceof Zend_Form_Decorator_Marker_File_Interface) {
-                return true;
+
+        $this->setElementDecorators(array(
+            array('FieldSize'),
+            array('ViewHelper'),
+            array('Addon'),
+            array('ElementErrors'),
+            array('Description', array('tag' => 'span', 'class' => 'help-block')),
+            array('HtmlTag', array('tag' => 'div', 'class' => 'col-'.$this->_getColType().'-' . $this->_getFieldColSize())),
+            array('Label', array('class' => 'col-' . $this->_getColType() . '-' . $this->_getLabelColSize() . ' control-label')),
+            array('Wrapper')
+        ));
+
+        parent::__construct($options);
+
+        foreach ($this->getElements() as $element) {
+            if ($element instanceof Zend_Form_Element_Checkbox) {
+                $decorators = $element->getDecorators();
+                $htmlTagDecorator = $decorators['Zend_Form_Decorator_HtmlTag'];
+                $class = $htmlTagDecorator->getOption('class');
+                $htmlTagDecorator->setOption('class', "checkbox $class");
+                $element->addDecorator($htmlTagDecorator);
+
+            } elseif($element instanceof Zend_Form_Element_Submit
+                    or $element instanceof Zend_Form_Element_Button
+                    or $element instanceof Zend_Form_Element_Image)
+            {
+                $decorators = $element->getDecorators();
+                $htmlTagDecorator = $decorators['Zend_Form_Decorator_HtmlTag'];
+                $class = $htmlTagDecorator->getOption('class');
+                $htmlTagDecorator->setOption('class', "col-" . $this->_getColType() . "-offset-" . $this->_getLabelColSize() . " $class");
+                $element->addDecorator($htmlTagDecorator);
             }
         }
 
-        return false;
+        $this->_displayConsecutiveButtonsOnTheSameLine();
+    }
+
+    protected function _displayConsecutiveButtonsOnTheSameLine()
+    {
+        $elements = $this->getElements();
+
+        $previousElement = null;
+        foreach ($elements as $element) {
+            if (null !== $previousElement) {
+                $this->_manageDecoratorsForButtons($previousElement, $element);
+            }
+
+            $previousElement = $element;
+        }
+    }
+
+    protected function _manageDecoratorsForButtons($current, $next)
+    {
+        if($current instanceof Zend_Form_Element_Submit
+                    or $current instanceof Zend_Form_Element_Button)
+        {
+            if(($next instanceof Zend_Form_Element_Submit
+                            or $next instanceof Zend_Form_Element_Button))
+            {
+                $currentHtmlTagDecorator = $current->getDecorator('Zend_Form_Decorator_HtmlTag');
+                if (!$currentHtmlTagDecorator->getOption('closeOnly')) {
+                    $currentHtmlTagDecorator->setOption('openOnly', true);
+                    $current->addDecorator($currentHtmlTagDecorator);
+                    $currentWrapperTagDecorator = $current->getDecorator('Twitter_Bootstrap_Form_Decorator_Wrapper');
+                    $currentWrapperTagDecorator->setOption('openOnly', true);
+                    $current->addDecorator($currentHtmlTagDecorator);
+                } else {
+                    $current->removeDecorator('Zend_Form_Decorator_HtmlTag');
+                    $current->removeDecorator('Twitter_Bootstrap_Form_Decorator_Wrapper');
+                }
+
+                $nextHtmlTagDecorator = $next->getDecorator('Zend_Form_Decorator_HtmlTag');
+                if ($nextHtmlTagDecorator) {
+                    $nextHtmlTagDecorator->setOption('closeOnly', true);
+                    $next->addDecorator($nextHtmlTagDecorator);
+                    $nextWrapperDecorator = $next->getDecorator('Twitter_Bootstrap_Form_Decorator_Wrapper');
+                    $nextWrapperDecorator->setOption('closeOnly', true);
+                    $next->addDecorator($nextHtmlTagDecorator);
+                }
+            }
+        }
     }
 }
